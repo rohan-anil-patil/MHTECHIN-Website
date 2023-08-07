7dd25de06bde8ed7ef804b18cd6f333062fd6edc
Gaurav Borse <gaurav.borse6677@gmail.com>
2023-08-07 18:05:42 +0530

    About page committed by Gaurav,( Kiran and Aniket)

---
+++ scriptm.js
@@ -0,0 +1,13 @@
+// navbar section
+// ===Navbar section===
+// document.querySelector(".menu-toggle").addEventListener("click", function () {
+//     document.querySelector("nav ul").classList.toggle("menu-open");
+// });
+// ===Navbar section===
+document.querySelector(".menu-toggle").addEventListener("click", function () {
+    document.querySelector("nav ul").classList.toggle("menu-open");
+  });
+
+
+    
+
